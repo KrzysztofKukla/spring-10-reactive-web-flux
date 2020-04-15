bf37aad92a92be5cbf749878ea664e4351c2836e
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-04-15 16:09:03 +0200

    Initial commit

---
+++ src/main/java/pl/kukla/krzys/reactivewebflux/Spring10ReactiveWebFluxApplication.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.reactivewebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring10ReactiveWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring10ReactiveWebFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kukla/krzys/reactivewebflux/Spring10ReactiveWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.reactivewebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring10ReactiveWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
